61ec71e86ba66a26de0ec48263e554b79143fc8a
David Pfeffer <bytenik@bytenik.com>
2012-12-23 15:17:29 -0500

    added more overloads to publish

--- src/PushoverQ/IBus.cs
+++ src/PushoverQ/IBus.cs
@@ -10,7 +10,14 @@ namespace PushoverQ
     public interface IBus
     {
         Task Publish(object message);
+        Task Publish(object message, TimeSpan timeout);
+        Task Publish(object message, CancellationToken token);
+        Task Publish(object message, TimeSpan timeout, CancellationToken token);
+
         Task Publish(object message, bool confirm);
+        Task Publish(object message, bool confirm, TimeSpan timeout);
+        Task Publish(object message, bool confirm, CancellationToken token);
+        Task Publish(object message, bool confirm, TimeSpan timeout, CancellationToken token);
 
         Task<T> Publish<T>(object message);
         Task<T> Publish<T>(object message, TimeSpan timeout);
