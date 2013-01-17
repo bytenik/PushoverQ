f2ff4c17f1a27cb0d151b6e820bb022f54372d5e
Tomas Roos <ptomasroos@gmail.com>
2013-01-17 09:22:42 +0100

    Clean up usings / uncommented code

--- src/PushoverQ.Protobuf/BusProtobufSerializer.cs
+++ src/PushoverQ.Protobuf/BusProtobufSerializer.cs
@@ -1,35 +1,11 @@
 ﻿using System;
 using System.IO;
-using System.Threading;
 using ProtoBuf;
-
-
-        //public void Serialize<TEntity>(TEntity entity, Stream stream)
-        //{
-        //    // ProtoBuf must have non-zero files
-        //    stream.WriteByte(42);
-        //    Serializer.Serialize(stream, entity);
-        //}
-
-        //public TEntity Deserialize<TEntity>(Stream stream)
-        //{
-        //    var signature = stream.ReadByte();
-
-        //    if (signature != 42)
-        //        throw new InvalidOperationException("Unknown view format");
-
-        //    return Serializer.Deserialize<TEntity>(stream);
-        //}
 
 namespace PushoverQ.Protobuf
 {
     class BusProtobufSerializer : ISerializer
     {
-
-        public BusProtobufSerializer()
-        {
-        }
-
         public void Serialize(object obj, Stream stream)
         {
             // ProtoBuf must have non-zero files
